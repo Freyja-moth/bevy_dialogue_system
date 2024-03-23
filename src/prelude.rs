57229422a4e4f03bc21d708a482ea5767c495a5c
Freyja-moth <freyjamoth@gmail.com>
2024-03-23 09:50:18 +0000

    Updated prelude imports

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,13 +1,8 @@
-#[cfg(feature = "doc_testing")]
-pub use crate::plugin::CurrentAction;
 pub use crate::{
     dialogue::Dialogue,
-    plugin::DialoguePlugin,
-    section::{Sentence, TypeWriter},
-    sections::Paragraph,
+    paragraph::Paragraph,
+    plugin::{CurrentAction, DialoguePlugin},
+    sentence::{Sentence, TypeWriter},
 };
 pub(crate) use bevy::{ecs::system::RunSystemOnce, prelude::*};
-pub(crate) use std::{
-    collections::VecDeque,
-    ops::{Deref, DerefMut},
-};
+pub(crate) use std::{collections::VecDeque, fmt::Display};
